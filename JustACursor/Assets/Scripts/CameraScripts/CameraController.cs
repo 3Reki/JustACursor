42a2fadf5d7baa6f6b26dfd4a8b6c405fcb2a660
CorentinO <cocoog77@gmail.com>
2022-10-14 16:36:55 +0200

    Stole Mickael's Camera script

---
+++ JustACursor/Assets/Scripts/CameraScripts/CameraController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace CameraScripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        public Transform target;
+    
+        [SerializeField] private Vector3 localPositionToMove = new Vector3(0, 5, -15);
+        [SerializeField] private Vector3 localPositionToLook = new Vector3(0, -1, 5);
+        [SerializeField] private float movingSpeed = 0.02f;
+        [SerializeField] private float rotationSpeed = 0.1f;
+
+        private Vector3 wantedPosition;
+        private Quaternion wantedRotation;
+        private Vector3 currentPosition;
+
+        private void FixedUpdate()
+        {
+            wantedPosition = target.TransformPoint(localPositionToMove);
+            wantedPosition.y = target.position.y + localPositionToMove.y;
+            currentPosition = transform.position;
+            currentPosition = Vector3.Lerp(currentPosition, wantedPosition, movingSpeed);
+            transform.position = currentPosition;
+
+            wantedRotation = Quaternion.LookRotation(target.TransformPoint(localPositionToLook) - currentPosition);
+            transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, rotationSpeed);
+        }
+    }
+}
