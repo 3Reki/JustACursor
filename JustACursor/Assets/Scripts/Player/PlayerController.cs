7237f9fb5c127dd83e744c494baae49623d45030
Jolan <jolan.robert56@gmail.com>
2022-10-20 17:15:45 +0200

    PlayerDeviceHandler

--- JustACursor/Assets/Scripts/Player/PlayerController.cs
+++ JustACursor/Assets/Scripts/Player/PlayerController.cs
@@ -9,21 +9,18 @@ namespace Player
     [RequireComponent(typeof(PlayerInput))]
     public class PlayerController : MonoBehaviour
     {
-        private const string mouseScheme = "Keyboard & Mouse";
-
+        [SerializeField] private PlayerData playerData;
         [SerializeField] private PlayerMovement playerMovement;
         [SerializeField] private PlayerShoot playerShoot;
         [SerializeField] private PlayerDash playerDash;
         [SerializeField] private PlayerEnergy playerEnergy;
-        [SerializeField] private PlayerData playerData;
-        [SerializeField] private PlayerInput playerInput;
-        
+        [SerializeField] private PlayerDeviceHandler playerDeviceHandler;
+
         private PlayerInputs inputs;
         private Camera mainCamera;
         private Vector2 moveDirection;
         private Vector2 lookPosition;
-        private string previousScheme;
-        
+
         public PlayerData data => playerData;
         private bool isDashing => playerDash.isDashing;
 
@@ -32,18 +29,6 @@ namespace Player
             inputs.Enable();
             
             mainCamera = Camera.main;
-            previousScheme = playerInput.currentControlScheme;
-            aimMethod = MouseAim;
-        }
-
-        private void OnEnable()
-        {
-            playerInput.onControlsChanged += OnControlsChanged;
-        }
-
-        private void OnDisable()
-        {
-            playerInput.onControlsChanged -= OnControlsChanged;
         }
 
         private void Update() {
@@ -68,60 +53,33 @@ namespace Player
         }
 
         private void FixedUpdate() {
-            if (isDashing)
-                playerDash.HandleDash();
-            
-            if (playerDash.isFirstPhase)
-            {
-                return;
-            }
+            if (isDashing) playerDash.HandleDash();
+            if (playerDash.isFirstPhase) return;
             
             moveDirection = inputs.Player.Move.ReadValue<Vector2>().normalized;
             playerMovement.Move(moveDirection);
 
             if (inputs.Player.Shoot.IsPressed())
             {
-                aimMethod();
+                if (playerDeviceHandler.currentAimMethod == PlayerDeviceHandler.AimMethod.Mouse) MouseAim();
+                else GamepadAim();
             }
             else
             {
                 playerMovement.LookForward(moveDirection);
             }
         }
-
-        private void GamepadAim()
-        {
-            lookPosition = (Vector2) transform.position + inputs.Player.LookGamepad.ReadValue<Vector2>();
-            if (lookPosition != Vector2.zero)
-            {
-                playerMovement.LookAtPosition(lookPosition);
-            }
-        }
-
+        
         private void MouseAim()
         {
             lookPosition = mainCamera.ScreenToWorldPoint(inputs.Player.LookMouse.ReadValue<Vector2>());
             playerMovement.LookAtPosition(lookPosition);
         }
-
-        private void OnControlsChanged(PlayerInput input)
+        
+        private void GamepadAim()
         {
-            if (playerInput.currentControlScheme == mouseScheme && previousScheme != mouseScheme)
-            {
-                Cursor.visible = true;
-                aimMethod = MouseAim;
-            }
-            else
-            {
-                Cursor.visible = false;
-                aimMethod = GamepadAim;
-            }
-
-            previousScheme = playerInput.currentControlScheme;
+            lookPosition = (Vector2) transform.position + inputs.Player.LookGamepad.ReadValue<Vector2>();
+            if (lookPosition != Vector2.zero) playerMovement.LookAtPosition(lookPosition);
         }
-
-        private delegate void ControllerCallback();
-
-        private ControllerCallback aimMethod;
     }
 }
