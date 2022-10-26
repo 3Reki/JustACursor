8dc14362f8034422e97cf49b3641891d4a14d868
Jolan <jolan.robert56@gmail.com>
2022-10-26 12:45:16 +0200

    Health Script

---
+++ JustACursor/Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    private int currentHealth;
+    
+    public UnityEvent onHealthGain;
+    public UnityEvent onHealthLose;
+    public UnityEvent onDeath;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void GainHealth(int amount)
+    {
+        currentHealth = Math.Clamp(currentHealth + amount, 0, maxHealth);
+        onHealthGain.Invoke();
+    }
+
+    public void LoseHealth(int amount)
+    {
+        currentHealth = Math.Clamp(currentHealth - amount, 0, maxHealth);
+        onHealthLose.Invoke();
+        
+        if (currentHealth == 0) onDeath.Invoke();
+    }
+
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+}
