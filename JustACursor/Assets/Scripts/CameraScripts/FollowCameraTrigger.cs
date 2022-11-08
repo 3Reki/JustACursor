6cec442a751c6247776239f12fb08f7ae19cf864
CorentinO <cocoog77@gmail.com>
2022-11-08 15:18:31 +0100

    Camera Triggers gizmos

--- JustACursor/Assets/Scripts/CameraScripts/FollowCameraTrigger.cs
+++ JustACursor/Assets/Scripts/CameraScripts/FollowCameraTrigger.cs
@@ -1,6 +1,10 @@
 using DG.Tweening;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace CameraScripts
 {
     public class FollowCameraTrigger : MonoBehaviour
@@ -14,5 +18,28 @@ namespace CameraScripts
             CameraController.instance.enabled = true;
             CameraController.mainCamera.DOOrthoSize(viewSize, movementDuration);
         }
+        
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Vector3 position = transform.position;
+            Vector2 colliderSize = GetComponent<BoxCollider2D>().size;
+            
+            Gizmos.color = new Color(1, .4f, .4f);
+            Gizmos.DrawWireCube(position + new Vector3(0, 0, -1), colliderSize);
+            Gizmos.color = new Color(1, .15f, .15f, .7f);
+            Gizmos.DrawCube(position, colliderSize);
+            
+            var style = new GUIStyle
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontStyle = FontStyle.Bold,
+                normal = new GUIStyleState {textColor = Color.white}
+            };
+
+            Handles.Label(position + new Vector3(0, 0, -2), "FollowCameraTrigger", style);
+
+        }
+#endif
     }
 }
