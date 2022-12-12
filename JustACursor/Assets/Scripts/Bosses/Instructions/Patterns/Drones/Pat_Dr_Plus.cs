d19ad8c14415ac0334523a7db1632032bf416fa9
Rayki <cocoog77@gmail.com>
2022-12-12 07:09:20 +0100

    Pat Dr Plus and Cross, + editors

---
+++ JustACursor/Assets/Scripts/Bosses/Instructions/Patterns/Drones/Pat_Dr_Plus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bosses.Instructions.Patterns.Drones
+{
+    
+    [CreateAssetMenu(fileName = "Pat_Dr_Plus", menuName = "Just A Cursor/Pattern/Drones/Plus Pattern", order = 0)]
+    public class Pat_Dr_Plus : Pattern<BossSound>
+    {
+        
+        public override void Play(BossSound entity)
+        {
+            base.Play(entity);
+
+            int droneCount = linkedEntity.droneCount;
+
+            for (int i = 0; i < droneCount; i++)
+            {
+                linkedEntity.GetDrone(i).SetPositionAndRotation(GetPosition(i, out Quaternion rotation),
+                    rotation);
+            }
+        }
+
+        public override void Stop() {}
+
+        private Vector2 GetPosition(int index, out Quaternion rotation)
+        {
+            Vector2 start;
+            Vector2 end;
+            switch (index / 3)
+            {
+                case 0 :
+                    start = linkedEntity.mover.room.topLeft;
+                    end = linkedEntity.mover.room.topRight;
+                    rotation = Quaternion.Euler(0, 0, 180);
+                    break;
+                case 1 :
+                    start = linkedEntity.mover.room.topRight;
+                    end = linkedEntity.mover.room.bottomRight;
+                    rotation = Quaternion.Euler(0, 0, 90);
+                    break;
+                case 2 :
+                    start = linkedEntity.mover.room.bottomRight;
+                    end = linkedEntity.mover.room.bottomLeft;
+                    rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+                case 3 :
+                    start = linkedEntity.mover.room.bottomLeft;
+                    end = linkedEntity.mover.room.topLeft;
+                    rotation = Quaternion.Euler(0, 0, -90);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return Vector2.Lerp(start, end, (index % 3 + 5f) / 12);
+        }
+    }
+}
---
+++ JustACursor/Assets/Scripts/Editor/Pat_Dr_PlusEditor.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Bosses.Instructions.Patterns.Drones;
+using LD;
+using UnityEditor;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(Pat_Dr_Plus))]
+    public class Pat_Dr_PlusEditor : UnityEditor.Editor
+    {
+        private void OnEnable()
+        {
+            SceneView.duringSceneGui += OnSceneGUI;
+        }
+
+        private void OnDisable()
+        {
+            SceneView.duringSceneGui -= OnSceneGUI;
+        }
+
+        private void OnSceneGUI(SceneView sceneView)
+        {
+            serializedObject.Update();
+            var room = FindObjectOfType<Room>();
+            if (!room)
+            {
+                return;
+            }
+
+            Vector3 topLeftPos;
+            Vector3 bottomRightPos;
+            {
+                var roomSO = new SerializedObject(room);
+                var topLeftCorner = (Transform) roomSO.FindProperty("roomTopLeftCorner").objectReferenceValue;
+                var bottomRightCorner = (Transform) roomSO.FindProperty("roomBottomRightCorner").objectReferenceValue;
+
+                if (!topLeftCorner || !bottomRightCorner)
+                {
+                    return;
+                }
+
+                topLeftPos = topLeftCorner.position;
+                bottomRightPos = bottomRightCorner.position;
+            }
+
+            DrawWirePattern(topLeftPos, bottomRightPos);
+        }
+
+        private void DrawWirePattern(Vector3 topLeftPos, Vector3 bottomRightPos)
+        {
+            Handles.color = Color.red;
+
+            Vector2 start = Vector2.zero;
+            Vector2 end = Vector2.zero;
+            Quaternion rotation = Quaternion.identity;
+            for (int i = 0; i < 12; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    GetPosition(out start, out end, out rotation, i, topLeftPos, bottomRightPos);
+                    Handles.DrawLine(Vector3.Lerp(start, end, 5f / 12), Vector3.Lerp(start, end, 7f / 12), 3);
+                }
+
+                Handles.DrawSolidDisc(Vector3.Lerp(start, end, (i % 3 + 5f) / 12), Vector3.back,
+                    (end - start).magnitude * 0.008f);
+                Handles.ArrowHandleCap(
+                    0,
+                    Vector3.Lerp(start, end, (i % 3 + 5f) / 12),
+                    rotation,
+                    1,
+                    EventType.Repaint
+                );
+            }
+        }
+
+        private static void GetPosition(out Vector2 start, out Vector2 end, out Quaternion rotation, int index,
+            Vector3 topLeft, Vector3 bottomRight)
+        {
+            Vector2 bottomLeft = new Vector2(topLeft.x, bottomRight.y);
+            Vector2 topRight = new Vector2(bottomRight.x, topLeft.y);
+            switch (index / 3)
+            {
+                case 0:
+                    start = topLeft;
+                    end = topRight;
+                    rotation = Quaternion.LookRotation(Vector3.down, Vector3.back);
+                    break;
+                case 1:
+                    start = topRight;
+                    end = bottomRight;
+                    rotation = Quaternion.LookRotation(Vector3.left, Vector3.back);
+                    break;
+                case 2:
+                    start = bottomRight;
+                    end = bottomLeft;
+                    rotation = Quaternion.LookRotation(Vector3.up, Vector3.back);
+                    break;
+                case 3:
+                    start = bottomLeft;
+                    end = topLeft;
+                    rotation = Quaternion.LookRotation(Vector3.right, Vector3.back);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
