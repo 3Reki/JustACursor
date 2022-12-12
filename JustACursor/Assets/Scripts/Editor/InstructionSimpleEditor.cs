56764dafc465fedca4fda14412c7924ab7a73a16
CorentinO <cocoog77@gmail.com>
2022-12-12 18:31:25 +0100

    Instruction editors (+ pat_composite)

---
+++ JustACursor/Assets/Scripts/Editor/InstructionSimpleEditor.cs
@@ -0,0 +1,47 @@
+﻿using Bosses.Instructions;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+namespace Editor
+{
+    [CustomEditor(typeof(InstructionSimple<>))]
+    public class InstructionSimpleEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_resolver, m_pattern;
+        private float patternDuration;
+
+        private void OnEnable()
+        {
+            m_resolver = serializedObject.FindProperty("resolver");
+            m_pattern = serializedObject.FindProperty("pattern");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            
+            patternDuration = 0;
+
+            {
+                Object objRef = m_pattern.objectReferenceValue;
+                
+                if (objRef != null)
+                    patternDuration = new SerializedObject(objRef).FindProperty("patternDuration").floatValue;
+            }
+
+            EditorGUILayout.PropertyField(m_pattern);
+            EditorGUILayout.PropertyField(m_resolver);
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.FloatField("Pattern Duration", patternDuration);
+            EditorGUI.EndDisabledGroup();
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+    
+    [CustomEditor(typeof(InstructionSimpleBoss))]
+    public class InstructionSimpleBossEditor : InstructionSimpleEditor {}
+    
+    [CustomEditor(typeof(InstructionSimpleDrones))]
+    public class InstructionSimpleDronesEditor : InstructionSimpleEditor {}
+}
---
+++ JustACursor/Assets/Scripts/Editor/Pat_CompositeEditor.cs
@@ -0,0 +1,46 @@
+﻿using Bosses.Instructions;
+using Bosses.Instructions.Patterns;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+namespace Editor
+{
+    [CustomEditor(typeof(Pat_Composite))]
+    public class Pat_CompositeEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_patterns;
+        private float patternDuration;
+
+        private void OnEnable()
+        {
+            m_patterns = serializedObject.FindProperty("patterns");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            
+            patternDuration = 0;
+            for (int i = 0; i < m_patterns.arraySize; i++)
+            {
+                Object objRef = m_patterns.GetArrayElementAtIndex(i).objectReferenceValue;
+                
+                if (objRef == null)
+                    continue;
+
+                float currentPatternDuration = new SerializedObject(objRef).FindProperty("patternDuration").floatValue;
+                if (currentPatternDuration > patternDuration)
+                {
+                    patternDuration = currentPatternDuration;
+                }
+            }
+
+            EditorGUILayout.PropertyField(m_patterns);
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.FloatField("Pattern Duration", patternDuration);
+            EditorGUI.EndDisabledGroup();
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
