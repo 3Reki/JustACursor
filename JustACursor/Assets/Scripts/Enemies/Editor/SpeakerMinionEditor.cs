9c58bdc5951fd63b78b3750aeb84c33cea558cec
Jolan <jolan.robert56@gmail.com>
2023-01-09 19:28:53 +0100

    SpeakerMinionEditor

---
+++ JustACursor/Assets/Scripts/Enemies/Editor/SpeakerMinionEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Enemies.Editor
+{
+    [CustomEditor(typeof(SpeakerMinion))]
+    public class SpeakerMinionEditor : UnityEditor.Editor
+    {
+        private SpeakerMinion minion;
+    
+        private void OnEnable() {
+            minion = target as SpeakerMinion;
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            minion.IsActiveAtStart = EditorGUILayout.Toggle("Is Active At Start", minion.IsActiveAtStart);
+
+            if (minion.IsActiveAtStart)
+            {
+                DrawDefaultInspector();
+            }
+
+            serializedObject.ApplyModifiedProperties();
+            
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(minion);
+                EditorSceneManager.MarkSceneDirty(minion.gameObject.scene);
+            }
+        }
+    }
+}
