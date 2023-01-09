64e35ad64c8ed11bba98199ccf44c1011e6d5b28
CorentinO <cocoog77@gmail.com>
2023-01-09 13:43:57 +0100

    Composite Pattern

--- JustACursor/Assets/Scripts/Bosses/Patterns/Pat_Composite.cs
+++ JustACursor/Assets/Scripts/Bosses/Patterns/Pat_Composite.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
+using XNode;
 
 namespace Bosses.Patterns
 {
     [CreateAssetMenu(fileName = "Pat_Composite", menuName = "Just A Cursor/Pattern/Composite")]
     public class Pat_Composite : Pattern<Boss>
     {
+        [Input(dynamicPortList = true, connectionType = ConnectionType.Override, backingValue = ShowBackingValue.Unconnected)] 
         [SerializeField] private Pattern<Boss>[] patterns;
         
         public override void Play(Boss entity)
         {
             base.Play(entity);
+
+            {
+                int i = 0;
+                foreach (NodePort dynamicInput in DynamicInputs)
+                {
+                    patterns[i] = (Pattern<Boss>) dynamicInput.Connection.node;
+                    i++;
+                }
+            }
             
             foreach (Pattern<Boss> pattern in patterns)
             {
